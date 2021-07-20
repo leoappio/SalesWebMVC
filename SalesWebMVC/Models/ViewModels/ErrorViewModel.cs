7fab76208813cd0b2a245e29c4075ed503698ad0
Leonardo Appio <leonardo.appio@gmail.com>
2021-07-20 10:09:20 -0300

    Primeiro Model View Controller - Department

---
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
